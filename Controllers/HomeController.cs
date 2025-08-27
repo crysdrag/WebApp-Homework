6ebf38ecfcbdf0fdd5ae53e1e9a93e465f17874c
tuyetmai <tuyetmai@TUYTMAI5CD0>
2025-08-28 00:02:27 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "This is Index page of HomeController";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewBag.Message = "This is Privacy page of HomeController";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ProductList()
+        {
+            ViewBag.Message = "This is ProductList page of ProductController";
+            return View();
+        }
+
+        public IActionResult Details(int id = 1)
+        {
+            ViewBag.Message = $"This is Details of Item ID = {id}";
+            return View();
+        }
+    }
+}
